87ec01a3ceb50420a1c3c8ef71055432f8ce5803
f198 <50043309+f198@users.noreply.github.com>
2019-04-27 10:47:21 +0100

    Add files via upload

---
+++ Coursera_Capstone_Notebook.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# This notebook is for the battle of neighbourhoods captsone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course')
+
